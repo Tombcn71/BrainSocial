a426f5b6612a1f7c7405acc23c6c90ffdbb5cacc
Tom <tbvanreijn@gmail.com>
2025-04-24 10:16:38 +0200

    nu

--- app/privacy-policy/page.tsx
+++ app/privacy-policy/page.tsx
@@ -11,7 +11,7 @@ export default function PrivacyPolicyPage() {
               <BrainCircuitIcon className="h-5 w-5 text-white" />
             </div>
             <span className="text-xl font-bold text-brand-purple">
-              BrainSocial
+              BrainSocial.nl
             </span>
           </Link>
         </div>
